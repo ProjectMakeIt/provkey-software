3990da38157519a41c84c9e8dd18d3c87b4177ed
Blixa Morgan <blixa@projectmakeit.com>
2019-03-22 22:37:19 -0500

    add gadgets to menu

--- menus.py
+++ menus.py
@@ -44,28 +44,41 @@ def enable_wifi_network():
     cmd = "sudo ifconfig wifi0 up"
     os.system(cmd)
 
+def enable_gadget():
+    cmd = "echo '20980000.usb' | tee /sys/kernel/config/usb_gadget/provkey/UDC"
+    os.system(cmd)
+
+def disable_gadget():
+    cmd = "echo '' | tee /sys/kernel/config/usb_gadget/provkey/UDC"
+    os.system(cmd)
+
 def getMenus(shutdown):
     root = Menu()
     load = ProgressMenu('images/scale.bmp',50,root)
     setup = Menu()
     network = Menu()
     wifi = Menu()
+    usbNet = Menu()
     usb = Menu()
     info = Menu(True,4)
     root.addLine(MenuEntry('Setup',setup))
     root.addLine(MenuLine('Exit',shutdown))
     setup.addLine(MenuEntry('Status',info))
     setup.addLine(MenuEntry('Network',network))
+    setup.addLine(MenuEntry('Gadget',gadget))
     setup.addLine(MenuEntry('back',root))
     network.addLine(MenuEntry('Wifi',wifi))
-    network.addLine(MenuEntry('Usb',usb))
+    network.addLine(MenuEntry('Usb',usbNet))
     network.addLine(MenuEntry('back',setup))
     wifi.addLine(MenuLine('Wifi Off',disable_wifi_network))
     wifi.addLine(MenuLine('Wifi On',enable_wifi_network))
     wifi.addLine(MenuEntry('back',network))
-    usb.addLine(MenuLine('Usb Off',disable_usb_network))
-    usb.addLine(MenuLine('Usb On',enable_usb_network))
-    usb.addLine(MenuEntry('back',network))
+    usbNet.addLine(MenuLine('Usb Off',disable_usb_network))
+    usbNet.addLine(MenuLine('Usb On',enable_usb_network))
+    usbNet.addLine(MenuEntry('back',network))
+    usb.addLine(MenuLine('Enable Gadget',enable_gadget))
+    usb.addLine(MenuLine('Disable Gadget',disable_gadget))
+    usb.addLine(MenuEntry('back',setup))
     info.addLine(MenuCustom(ip_render),False)
     info.addLine(MenuCustom(cpu_render),False)
     info.addLine(MenuCustom(cmd_render),False)
