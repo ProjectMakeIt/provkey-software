38f757cfd5d87d9830fdca98b4c7dee5e5010337
Blixa Morgan <blixa@projectmakeit.com>
2019-03-22 20:37:37 -0500

    add network menus

--- menus.py
+++ menus.py
@@ -28,15 +28,40 @@ def disk_render():
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
     return subprocess.check_output(cmd, shell = True)
 
+def disable_usb_network():
+    cmd = "sudo ifconfig usb0 down"
+
+def enable_usb_network():
+    cmd = "sudo ifconfig usb0 up"
+
+def disable_wifi_network():
+    cmd = "sudo ifconfig wifi0 down"
+
+def enable_wifi_network():
+    cmd = "sudo ifconfig wifi0 up"
+
 def getMenus(shutdown):
     root = Menu()
     load = ProgressMenu('images/scale.bmp',50,root)
     setup = Menu()
+    network = Menu()
+    wifi = Menu()
+    usb = Menu()
     info = Menu(True,4)
     root.addLine(MenuEntry('Setup',setup))
     root.addLine(MenuLine('Exit',shutdown))
     setup.addLine(MenuEntry('Status',info))
+    setup.addLine(MenuEntry('Network',network))
     setup.addLine(MenuEntry('back',root))
+    network.addLine(MenuEntry('Wifi',wifi))
+    network.addLine(MenuEntry('Usb',usb))
+    network.addLine(MenuEntry('back',setup))
+    wifi.addLine(MenuLine('Wifi Off',disable_wifi_network))
+    wifi.addLine(MenuLine('Wifi On',enable_wifi_network))
+    wifi.addLine(MenuEntry('back',network))
+    usb.addLine(MenuLine('Usb Off',disable_usb_network))
+    usb.addLine(MenuLine('Usb On',enable_usb_network))
+    usb.addLine(MenuEntry('back',network))
     info.addLine(MenuCustom(ip_render),False)
     info.addLine(MenuCustom(cpu_render),False)
     info.addLine(MenuCustom(cmd_render),False)
